e80babb8a5ee13b898bbecfc441dc07cef6c5a1a
pk10080615 <51924723+pk10080615@users.noreply.github.com>
2019-08-05 21:28:55 -0500

    Create on_my_own.py

---
+++ on_my_own.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+from time import sleep
+print ("This is a practice run. Use my code and others as a little refresher. If you're about to start coding, look through 'history' to remember the command line to do so.")
+sleep(3)
